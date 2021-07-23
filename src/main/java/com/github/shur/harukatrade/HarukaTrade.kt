d28107c8219952ce6592b656e8e943ebb43123a8
SigureRuri <50079507+SigureRuri@users.noreply.github.com>
2021-07-23 11:42:26 +0900

    雛型を追加

---
+++ src/main/java/com/github/shur/harukatrade/HarukaTrade.kt
@@ -0,0 +1,9 @@
+package com.github.shur.harukatrade
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class HarukaTrade : JavaPlugin() {
+    override fun onEnable() { }
+
+    override fun onDisable() { }
+}
